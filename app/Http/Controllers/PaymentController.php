91425a9fc24838c1e224121006c3da3bec3ada72
Yu-hanCheng <sarahcheng1231@gmail.com>
2019-10-05 11:28:53 +0800

    paypal test

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -8,6 +8,14 @@ use App\Exceptions\InvalidRequestException;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Http\Requests\ApplyRefundRequest;
+use ResultPrinter_by_s as ResultPrinter_by;
+use PayPal\Api\Amount;
+use PayPal\Api\Details;
+use PayPal\Api\Payment;
+use PayPal\Api\PaymentExecution;
+use PayPal\Api\Transaction;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Rest\ApiContext;
 
 class PaymentController extends Controller
 {
@@ -69,6 +77,46 @@ class PaymentController extends Controller
             }
             
             $obj->CheckOut();
+    }
+
+    public function PaypalExec(Request $request)
+    {
+        // require __DIR__ . '/../bootstrap.php';
+        $clientId = 'AUvyf-GcLIYKxkpfIPQ5gcGzKjwlPodvQvyww5OU4iwpLUTXlcy9dZq_t91toX1XE4-PR1oH2KA2h22A';
+        $clientSecret = 'EHIwO_yPRI_-8UY6olHCFuU2_9nh03qbFA1sGEomkYA-HAd2KgjogCBYUHMzoNPwThh1DNxS_PRNgulS';
+
+        $apiContext = getApiContext($clientId, $clientSecret);
+        include('ResultPrinter_by.php');
+        
+        
+        $out = new \Symfony\Component\Console\Output\ConsoleOutput();
+        $data = $request->all();
+        $out->writeln("data: ".json_encode($data['paymentId']));
+
+        $paymentId = $_GET['paymentId'];
+        $payment = Payment::get($paymentId, $apiContext);
+        $execution = new PaymentExecution();
+        $execution->setPayerId($_GET['PayerID']);
+
+            try {
+                $result = $payment->execute($execution, $apiContext);
+                ResultPrinter_by::printResult("Executed Payment", "Payment", $payment->getId(), $execution, $result);
+
+                try {
+                    $payment = Payment::get($paymentId, $apiContext);
+                } catch (Exception $ex) {
+                    // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+                    ResultPrinter_by::printError("Get Payment", "Payment", null, null, $ex);
+                    exit(1);
+                }
+            } catch (Exception $ex) {
+                // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+                ResultPrinter_by::printError("Executed Payment", "Payment", null, null, $ex);
+                exit(1);
+            }
+            ResultPrinter_by::printResult("Get Payment", "Payment", $payment->getId(), null, $payment);
+            
+            return $payment;
     }
 
     public function listenPayResult(Request $request)
@@ -180,4 +228,63 @@ class PaymentController extends Controller
         }  
         $obj->DoAction();
     }
+}
+
+
+/**
+ * Helper method for getting an APIContext for all calls
+ * @param string $clientId Client ID
+ * @param string $clientSecret Client Secret
+ * @return PayPal\Rest\ApiContext
+ */
+function getApiContext($clientId, $clientSecret)
+{
+
+    // #### SDK configuration
+    // Register the sdk_config.ini file in current directory
+    // as the configuration source.
+    /*
+    if(!defined("PP_CONFIG_PATH")) {
+        define("PP_CONFIG_PATH", __DIR__);
+    }
+    */
+
+
+    // ### Api context
+    // Use an ApiContext object to authenticate
+    // API calls. The clientId and clientSecret for the
+    // OAuthTokenCredential class can be retrieved from
+    // developer.paypal.com
+
+    $apiContext = new ApiContext(
+        new OAuthTokenCredential(
+            $clientId,
+            $clientSecret
+        )
+    );
+
+    // Comment this line out and uncomment the PP_CONFIG_PATH
+    // 'define' block if you want to use static file
+    // based configuration
+
+    $apiContext->setConfig(
+        array(
+            'mode' => 'sandbox',
+            'log.LogEnabled' => true,
+            'log.FileName' => '../PayPal.log',
+            'log.LogLevel' => 'DEBUG', // PLEASE USE `INFO` LEVEL FOR LOGGING IN LIVE ENVIRONMENTS
+            'cache.enabled' => true,
+            //'cache.FileName' => '/PaypalCache' // for determining paypal cache directory
+            // 'http.CURLOPT_CONNECTTIMEOUT' => 30
+            // 'http.headers.PayPal-Partner-Attribution-Id' => '123123123'
+            //'log.AdapterFactory' => '\PayPal\Log\DefaultLogFactory' // Factory class implementing \PayPal\Log\PayPalLogFactory
+        )
+    );
+
+    // Partner Attribution Id
+    // Use this header if you are a PayPal partner. Specify a unique BN Code to receive revenue attribution.
+    // To learn more or to request a BN Code, contact your Partner Manager or visit the PayPal Partner Portal
+    // $apiContext->addRequestHeader('PayPal-Partner-Attribution-Id', '123123123');
+
+    return $apiContext;
 }
